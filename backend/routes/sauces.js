b8c5ce10546384e96e3c5c7cde9a7605bcb1657a
LeMasne <alexislemasne@gmail.com>
2021-04-29 09:34:58 +0200

    Working tree

---
+++ backend/routes/sauces.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const auth = require("../middleware/auth");
+const multer = require("../middleware/multer-config");
+const saucesCtrl = require("../controllers/sauces");
+
+router.get("/", auth, saucesCtrl.getAllsauces);
+router.get("/:id", auth, saucesCtrl.getOneSauce);
+router.post("/", auth, multer, saucesCtrl.createSauce);
+router.put("/:id", auth, multer, saucesCtrl.modifySauce);
+// router.post("/:id/like", auth, multer, saucesCtrl.likeDislikeSauce);
+router.delete("/:id", auth, saucesCtrl.deleteSauce);
+
+module.exports = router;
